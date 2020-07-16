8de7d89456574f01ecee1b03969974c83f48b234
JDGoldman <josh@ikram.com>
2020-07-15 20:59:51 -0500

    Added Forms Authentication to Blazor App.

---
+++ src/WebFormsApplicationWithBlazorApplication/BlazorAppWithFormsAuthentication/CustomAuthStateProvider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.DataProtection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+
+namespace BlazorAppWithFormsAuthentication
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _configuration;
+
+        public CustomAuthStateProvider(IHttpContextAccessor httpContextAccessor, IConfiguration Configuration)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _configuration = Configuration;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+            ClaimsPrincipal user;
+
+            string cookieString = "";
+            var cookie_name = _configuration["CookieName"];
+            var application_name = _configuration["ApplicationName"];
+            
+            var result = _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue(cookie_name, out cookieString);
+            if (result)
+            {
+                var shared_key_location = _configuration["SharedKeyFileLocation"];
+                var provider = DataProtectionProvider.Create(new System.IO.DirectoryInfo(shared_key_location),
+                (builder) =>
+                {
+                    builder.SetApplicationName(application_name);
+                    //builder.ProtectKeysWithDpapi(false);
+                });
+
+                var dataProtector = provider.CreateProtector("Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationMiddleware", cookie_name, "v2");
+                var ticketFormat = new Microsoft.AspNetCore.Authentication.TicketDataFormat(dataProtector);
+                AuthenticationTicket ticket = ticketFormat.Unprotect(cookieString);
+                if (ticket != null) {
+                    user = ticket.Principal;
+                } else {
+                    var identity = new ClaimsIdentity();
+                    user = new ClaimsPrincipal(identity);
+
+                }
+            }
+            else
+            {
+                var identity = new ClaimsIdentity();
+                user = new ClaimsPrincipal(identity);
+            }
+
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
--- src/WebFormsApplicationWithBlazorApplication/BlazorAppWithFormsAuthentication/Startup.cs
+++ src/WebFormsApplicationWithBlazorApplication/BlazorAppWithFormsAuthentication/Startup.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.ComponentModel;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using BlazorAppWithFormsAuthentication.Data;
 
 namespace BlazorAppWithFormsAuthentication
@@ -26,9 +35,44 @@ namespace BlazorAppWithFormsAuthentication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // HttpContextAccessor
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            services.AddHttpClient();
+            services.AddScoped<HttpClient>();
+
+            //added for authentication
+            var shared_key_location = Configuration["SharedKeyFileLocation"];
+            var cookie_name = Configuration["CookieName"];
+            var application_name = Configuration["ApplicationName"];
+            var domain_name = Configuration["DomainName"];
+            services.AddDataProtection()
+                .ProtectKeysWithDpapi().DisableAutomaticKeyGeneration()
+                .PersistKeysToFileSystem(new System.IO.DirectoryInfo(shared_key_location))
+                .SetApplicationName(application_name);
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    options.Cookie.Name = cookie_name;
+                    options.Cookie.Path = "/";
+                    options.Cookie.Domain = domain_name;
+                    options.Cookie.SameSite = SameSiteMode.Lax;
+                    options.Cookie.HttpOnly = false;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                    options.Cookie.IsEssential = true;
+                }
+                );
+            services.AddAuthorization();
+
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            //added for authentication
+            services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +93,12 @@ namespace BlazorAppWithFormsAuthentication
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            //added
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
